68740895d38875413008b428fa44404efa685e24
afeiship <1290657123@qq.com>
2021-08-03 20:41:29 +0800

    feat: use nx.fetch

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,10 +1,13 @@
-(function() {
+(function () {
   const NxRailsApiSchema = require('../src');
 
-  describe('NxRailsApiSchema.methods', function() {
-    test('init', function() {
-      const data = { key: 1, value: 2 };
-      expect(!!data).toBe(true);
+  jest.setTimeout(50 * 1000);
+
+  describe('NxRailsApiSchema.methods', function () {
+    test('init', function (done) {
+      // const data = { key: 1, value: 2 };
+      // expect(!!data).toBe(true);
+      var schema = new NxRailsApiSchema();
     });
   });
 })();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 (function () {
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-  var nodeHttpSchema = require('node-http-schema').default;
-  var defaults = { context: global };
+  var nodeFetch = require('node-fetch');
+  var defaults = { fetch: nodeFetch, external: { baseUrl: null, username: null, password: null } };
+  var STD_UA =
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+
+  require('@afeiship/next-jwt-authorization');
+  require('@jswork/next-fetch');
 
   var NxRailsApiSchema = nx.declare('nx.RailsApiSchema', {
-    statics: {
-      create: function (inConfig, inOptions) {
-        return nodeHttpSchema(inConfig, inOptions);
+    methods: {
+      init: function (inOptions) {
+        this.options = nx.mix(null, defaults, inOptions);
+        this.external = this.options.external;
+        this.token = null;
+      },
+      auth: function () {
+        const { baseUrl, username, password } = this.external;
+        const url = `${baseUrl}/rails_jwt_admin/authentication`;
+        if (this.token) return Promise.resolve(this.token);
+        return new Promise(function (resolve, reject) {
+          nx.JwtAuthorization.auth(url, { username, password })
+            .then((res) => resolve(res.token))
+            .catch(reject);
+        });
+      },
+      request: function (inMethod, inUrl, inData, inOptions) {
+        var self = this;
+        return self.auth().then(() => {
+          inOptions.headers = { Authorization: self.token, 'User-Agent': STD_UA };
+          return this.$base.request(inMethod, inUrl, inData, inOptions);
+        });
       }
     }
   });
