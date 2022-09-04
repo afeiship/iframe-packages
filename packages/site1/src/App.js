3bdc1ca029d5d9a532165397f66b83b690ca1948
afeiship <1290657123@qq.com>
2022-09-04 22:17:42 +0800

    feat: upt site1 ift

--- packages/site1/src/App.js
+++ packages/site1/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
+import { About } from './pages/about';
+import { IframeApp } from './components/iframe-app';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const ift = nx.Json2base64.decode(nx.qs().ift);
+    console.log('ift:', ift);
+  }, []);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <IframeApp src="http://localhost:5002/" />
     </div>
   );
 }
---
+++ packages/site1/src/components/iframe-app.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.iframe`
+  width: 80%;
+  height: 80vh;
+  margin: 0 auto;
+`;
+
+export const IframeApp = (props) => {
+  return <Container src={props.src} />;
+};
