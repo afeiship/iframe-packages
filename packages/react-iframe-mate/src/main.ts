41a6ae9e99e73ff923d6a8b9a87b5e5da95c39f8
afeiship <1290657123@qq.com>
2022-09-11 15:20:02 +0800

    fix: rm useNavigation

--- packages/react-iframe-mate/src/main.ts
+++ packages/react-iframe-mate/src/main.ts
@@ -1,6 +1,5 @@
 import Component from './components';
 import useIfm from './components/use-ifm';
-import { useNavigation } from './components/navigation-provider';
 
 export default Component;
-export { useIfm, useNavigation };
+export { useIfm };
