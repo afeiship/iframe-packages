53a0f5a16f5d447fd3d38a5b6522006694bd2e04
afeiship <1290657123@qq.com>
2022-10-16 13:35:14 +0800

    feat: mv to->sub

--- packages/react-iframe-mate/src/components/ifm-link.tsx
+++ packages/react-iframe-mate/src/components/ifm-link.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, LinkProps } from 'react-router-dom';
 import { useIfm } from '.';
 
-export interface IfmLinkProps {
+export interface IfmLinkProps extends LinkProps {
   path: string;
-  sub: string;
+  to: string;
   referer: string;
   children: React.ReactNode;
   onClick?: (e: React.MouseEvent) => void;
 }
 
 export const IfmLink = (props: IfmLinkProps) => {
-  const { path, sub, children, referer, onClick, ...rest } = props;
+  const { path, children, referer, onClick, to, ...rest } = props;
   const ifm = useIfm()!.ifm;
   const isMate = ifm.role !== 'standalone';
 
   const handleClick = (e: React.MouseEvent) => {
     ifm.navigate({
       path,
-      sub,
+      sub: to,
       referer,
       replace: false
     });
@@ -26,7 +26,7 @@ export const IfmLink = (props: IfmLinkProps) => {
   };
 
   return (
-    <Link to={sub} replace={isMate} {...rest} onClick={handleClick}>
+    <Link to={to} replace={isMate} {...rest} onClick={handleClick}>
       {children}
     </Link>
   );
