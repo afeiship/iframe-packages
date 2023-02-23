cfc163bf8ca164401c35351d16245a25fe7cefd2
afeiship <1290657123@qq.com>
2023-02-23 10:39:43 +0800

    feat: click not trigger when first click

--- packages/react-iframe-mate/src/components/app-path-link.tsx
+++ packages/react-iframe-mate/src/components/app-path-link.tsx
@@ -1,26 +1,27 @@
 import { useIfm } from '.';
 import React, { useEffect, useState, ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-// https://cloud.beta.aigene.org.cn/main/projects/15/jobs/1073
-// ?app-path=/protein-solubizing-frontend/beta/index.html
-// #/datasets/1073/report/3476?showId=1073_62470
+// https://cloud.beta.aigene.org.cn/main/projects/15/jobs/1073?app-path=/protein-solubizing-frontend/beta/index.html#/datasets/1073/report/3476?showId=1073_62470
 
 type Props = {
   path: string;
   subpath?: string;
   to: string;
   children: ReactNode;
+  replace?: boolean;
   onClick?: (e: React.MouseEvent) => void;
 };
 
 export const AppPathLink = (inProps: Props) => {
-  const { path, subpath, to, children, onClick } = inProps;
+  const { path, subpath, to, children, replace, onClick } = inProps;
   const { ifm } = useIfm()!;
   const [ori, setOri] = useState<string>();
   const [clicked, setClicked] = useState(false);
   const [targetURL, setTargetURL] = useState<string>('');
-  const appPathStr = encodeURIComponent(encodeURIComponent((`${subpath || ''}#${to}`)));
+  const appPathStr = encodeURIComponent(encodeURIComponent(`${subpath}#${to}`));
+  const navigate = useNavigate();
+  const isReplace = replace === true || typeof replace === 'undefined';
 
   const handleClick = (inEvent: any) => {
     inEvent.preventDefault();
@@ -33,7 +34,6 @@ export const AppPathLink = (inProps: Props) => {
     ifm.post({ command: 'url' }).then((res) => {
       const uri = new URL(res);
       setOri(uri.origin);
-
     });
   }, [ifm]);
 
@@ -42,7 +42,7 @@ export const AppPathLink = (inProps: Props) => {
   }, [ori, path, appPathStr]);
 
   if (!targetURL) return null;
-  if (clicked) return <Link to={to}>{children}</Link>;
+  if (clicked) return navigate(to, { replace: isReplace }), null;
 
   return (
     <a href={targetURL} onClick={handleClick}>
